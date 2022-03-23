10614cf2d4a0174a687179b08c612767ab169733
EmmanuelMejiaDeveloper <100601601+EmmanuelMejiaDeveloper@users.noreply.github.com>
2022-03-23 00:15:05 -0600

    ejercicios Terminados
    Ejercicios

---
+++ pages/validar/validacio1.php
@@ -0,0 +1,41 @@
+<?php
+$res1 = $_POST['res1'];
+$res2 = $_POST['res2'];
+$res3 = $_POST['res3'];
+$res4 = $_POST['res4'];
+$res5 = $_POST['res5'];
+$res6 = $_POST['res6'];
+$res7 = $_POST['res7'];
+$suma=0;
+
+if($res1 == 56){
+  $suma=$suma+1;
+}
+
+if($res2 == 14){
+  $suma=$suma+1;
+}
+
+if($res3 == 625){
+  $suma=$suma+1;
+}
+
+if($res4 == 7.5){
+  $suma=$suma+1;
+}
+
+if($res5 == "6/8"){
+  $suma=$suma+1;
+}
+
+if($res6 == 54){
+  $suma=$suma+1;
+}
+
+if($res7 == 5){
+  $suma=$suma+1;
+}
+
+echo '<script>alert("Tienes '.$suma.' aciertos de 7");</script>';
+echo '<script> window.location="../ejercicio1.html"; </script>';
+ ?>
---
+++ pages/validar/validacio2.php
@@ -0,0 +1,39 @@
+<?php
+$res1 = $_POST['res1'];
+$res2 = $_POST['res2'];
+$res3 = $_POST['res3'];
+$res4 = $_POST['res4'];
+$res5 = $_POST['res5'];
+$res6 = $_POST['res6'];
+$suma=0;
+
+if($res1 == 887503681 or $res1 == "887,503,681"){
+  $suma=$suma+1;
+}
+
+if($res2 == 14){
+  $suma=$suma+1;
+}
+
+if($res3 == 18){
+  $suma=$suma+1;
+}
+
+if($res4 == 16.3){
+  $suma=$suma+1;
+}
+
+if($res5 == 6){
+  $suma=$suma+1;
+}
+
+if($res6 == 36.9047 or $res6 == 36.90 or $res6 == 36){
+  $suma=$suma+1;
+}
+
+
+
+
+echo '<script>alert("Tienes '.$suma.' aciertos de 6");</script>';
+echo '<script> window.location="../ejercicio2.html"; </script>';
+ ?>
